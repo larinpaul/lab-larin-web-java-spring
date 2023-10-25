0b906a3786d99428921e5d26083bee5b06ea6a09
Pavel Larin <www.larinpavel@mail.ru>
2023-10-25 17:13:50 +0500

    Added JPA and two entity classes

--- src/main/java/com/example/larinwebjavaspring/data/Registration.java
+++ src/main/java/com/example/larinwebjavaspring/data/Registration.java
@@ -1,5 +1,9 @@
 package com.example.larinwebjavaspring.data;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
 @Entity
 @Table(name = "registration")
 public class Registration {
@@ -21,8 +25,65 @@ public class Registration {
     @JoinColumn(name = "ownerId")
     private VehicleOwner owner;
 
-    // standard constructors / setters / getters / toString
+    // perhaps, re-generate standard constructors / setters / getters / toString
 
+    public Registration(Long registrationId, String number, LocalDate date, Vehicle vehicle, VehicleOwner owner) {
+        this.registrationId = registrationId;
+        this.number = number;
+        this.date = date;
+        this.vehicle = vehicle;
+        this.owner = owner;
+    }
 
+    public Long getRegistrationId() {
+        return registrationId;
+    }
+
+    public void setRegistrationId(Long registrationId) {
+        this.registrationId = registrationId;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public Vehicle getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(Vehicle vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public VehicleOwner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(VehicleOwner owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "Registration{" +
+                "registrationId=" + registrationId +
+                ", number='" + number + '\'' +
+                ", date=" + date +
+                ", vehicle=" + vehicle +
+                ", owner=" + owner +
+                '}';
+    }
 
 }
--- src/main/java/com/example/larinwebjavaspring/data/Vehicle.java
+++ src/main/java/com/example/larinwebjavaspring/data/Vehicle.java
@@ -1,5 +1,7 @@
 package com.example.larinwebjavaspring.data;
 
+import jakarta.persistence.*;
+
 @Entity
 @Table(name = "vehicle")
 public class Vehicle {
@@ -25,12 +27,74 @@ public class Vehicle {
     @JoinColumn(name = "colorId")
     private Color color;
 
-    // standard constructors / setters / getters / toString
+    // perhaps, re-generate standard constructors / setters / getters / toString
 
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "vehicleId=" + vehicleId +
+                ", yearOfManufacture=" + yearOfManufacture +
+                ", engineNumber='" + engineNumber + '\'' +
+                ", vehicleType=" + vehicleType +
+                ", vehicleModel=" + vehicleModel +
+                ", color=" + color +
+                '}';
+    }
 
+    public Long getVehicleId() {
+        return vehicleId;
+    }
 
+    public void setVehicleId(Long vehicleId) {
+        this.vehicleId = vehicleId;
+    }
 
+    public Integer getYearOfManufacture() {
+        return yearOfManufacture;
+    }
 
+    public void setYearOfManufacture(Integer yearOfManufacture) {
+        this.yearOfManufacture = yearOfManufacture;
+    }
 
+    public String getEngineNumber() {
+        return engineNumber;
+    }
 
+    public void setEngineNumber(String engineNumber) {
+        this.engineNumber = engineNumber;
+    }
+
+    public VehicleType getVehicleType() {
+        return vehicleType;
+    }
+
+    public void setVehicleType(VehicleType vehicleType) {
+        this.vehicleType = vehicleType;
+    }
+
+    public VehicleModel getVehicleModel() {
+        return vehicleModel;
+    }
+
+    public void setVehicleModel(VehicleModel vehicleModel) {
+        this.vehicleModel = vehicleModel;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Vehicle(Long vehicleId, Integer yearOfManufacture, String engineNumber, VehicleType vehicleType, VehicleModel vehicleModel, Color color) {
+        this.vehicleId = vehicleId;
+        this.yearOfManufacture = yearOfManufacture;
+        this.engineNumber = engineNumber;
+        this.vehicleType = vehicleType;
+        this.vehicleModel = vehicleModel;
+        this.color = color;
+    }
 }
